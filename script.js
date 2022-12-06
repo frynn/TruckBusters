3a767d6b063777a3e2f8f5375720f84ca2d14645
scriptSQD <scriipt.fun@gmail.com>
2022-12-06 21:53:50 +0300

    * Implement fullscreen burger menu

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//
+// Modals
+//
+
 // Modal toggles
 const service_modal_toggles = document.getElementById("services_modals");
 const service_modals_close_buttons = document.getElementsByClassName("modal_close");
@@ -17,4 +21,23 @@ for (const button of service_modals_close_buttons) {
         const dialog = evt.target.parentNode.parentNode;
         dialog.close();
     });
+}
+
+//
+// Mobile menu
+//
+
+const menu = document.getElementById("menu_mobile");
+const burger_button = document.getElementById("burger_button");
+
+// Listen to click events and toggle menu state
+burger_button.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+})
+
+const mobile_menu_links = document.getElementById("mobile_menu_links");
+for (const link of mobile_menu_links.children) {
+    link.children[0].addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    })
 }
